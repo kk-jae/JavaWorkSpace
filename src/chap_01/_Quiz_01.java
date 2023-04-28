5724e47118c66eacef4d12d21556fd7b5ccf7803
kk-jae <kk_jae@naver.com>
2023-04-28 15:10:39 +0900

    feat: Quiz_01, 연산자

--- src/chap_01/_Quiz_01.java
+++ src/chap_01/_Quiz_01.java
@@ -1,2 +1,20 @@
-package chap_01;public class _Quiz_01 {
+package chap_01;
+
+/*
+(실행 결과)
+상암08번 버스
+약 3분 후 도착
+남은 거리 1.2km
+ */
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNo = "상암08";; // 문자와 숫자가 결합된 형태
+        int minute = 3;
+        float distance = 1.2F;
+
+        System.out.println(busNo + "번 버스");
+        System.out.println("약 " + minute + "분 후 도착");
+        System.out.println("남은 거리 " + distance + "km");
+    }
 }
